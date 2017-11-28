e8944b5b859fdc612e6f27f28f94f22962e1605e
FlavioVieiraDeMacedo <f.m.kmacedo@hotmail.com>
2017-11-28 09:49:29 -0200

    msg email

--- send-mail.php
+++ send-mail.php
@@ -7,5 +7,5 @@
   $assunto = "Mensagem de ".$email.com
   mail($to,$assunto,$mensagem);
 ?-->
-
+<div><p>Não deixe de contactar, mande um email para f.m.kmacedo@hotmail.com</p></div>
 <img src="http://rcpfm.com/wp-content/uploads/2016/11/manutencao.jpg">
